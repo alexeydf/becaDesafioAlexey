58c5fcf6e8f02372a0792ad79ce6b0f8b90b1f3d
Alexey Lima Barreto <alimabar@emeal.nttdata.com>
2022-01-24 02:47:01 -0300

    Desafio 02

--- src/main/java/git/desafioalexey/pizzaria/controller/VendaController.java
+++ src/main/java/git/desafioalexey/pizzaria/controller/VendaController.java
@@ -36,6 +36,14 @@ public class VendaController {
     @PatchMapping("/atualizar/{id}")
     public ResponseEntity<Venda> atualizar(@RequestBody Venda venda,@PathVariable Long id) {
 
+        Pizza pizza = new Pizza(1L,"Chocolate", "Doce", 20.0);
+        ItemVenda item = new ItemVenda(1L, pizza, 2, 20.0);
+        ItemVenda item2 = new ItemVenda(1L, pizza, 5, 20.0);
+
+        venda.setItens(List.of(
+                item, item2
+        ));
+
         venda.setId(id);
 
         return ResponseEntity.ok(venda);
@@ -45,7 +53,15 @@ public class VendaController {
     public ResponseEntity<Venda> listarPorId(@PathVariable Long id) {
         Cliente cliente = new Cliente(1L,"Alexey", "Guara 1", "61 999996666", "a@l.com");
 
+
+        Pizza pizza = new Pizza(1L,"Chocolate", "Doce", 20.0);
+        ItemVenda item = new ItemVenda(1L, pizza, 2, 20.0);
+        ItemVenda item2 = new ItemVenda(1L, pizza, 5, 20.0);
+
         Venda venda = new Venda(id, new Date(),cliente);
+        venda.setItens(List.of(
+                item, item2
+        ));
 
         return ResponseEntity.ok(venda);
     }
@@ -55,9 +71,25 @@ public class VendaController {
         Cliente cliente1 = new Cliente(1L,"Alexey", "Guara 1", "61 999996666", "a@l.com");
         Cliente cliente2 = new Cliente(2L,"Thanan", "Guara 1", "61 999996669", "t@l.com");
 
+        Pizza pizza = new Pizza(1L,"Chocolate", "Doce", 20.0);
+        ItemVenda item = new ItemVenda(1L, pizza, 2, 20.0);
+        ItemVenda item2 = new ItemVenda(1L, pizza, 5, 20.0);
+
         Venda venda1 = new Venda(2L,new Date(),cliente1);
         Venda venda2 = new Venda(3L, new Date(),cliente2);
         Venda venda3 = new Venda(5L, new Date(),cliente1);
+
+
+        venda1.setItens(List.of(
+                item, item2
+        ));
+        venda2.setItens(List.of(
+                item
+        ));
+        venda3.setItens(List.of(
+                item2
+        ));
+
 
         List<Venda> vendas = List.of(venda1, venda2, venda3);
 
