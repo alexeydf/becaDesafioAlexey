0c75f8d5afd27e5d84b5c3e7f6d110fd99381fed
Alexey Lima Barreto <alimabar@emeal.nttdata.com>
2022-01-31 14:41:27 -0300

    Desafio 04

--- src/main/java/git/desafioalexey/pizzaria/models/Cliente.java
+++ src/main/java/git/desafioalexey/pizzaria/models/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -18,10 +19,10 @@ public class Cliente {
     private String email;
     private Integer comprasRealizadas;
     private Double totalGasto;
-    private Date dataCadastro;
+    private LocalDate dataCadastro;
 
     public Cliente() {
-        this.dataCadastro = new Date();
+        this.dataCadastro = LocalDate.now();
         this.totalGasto = 0.0;
         this.comprasRealizadas = 0;
     }
@@ -32,7 +33,7 @@ public class Cliente {
         this.endereco = endereco;
         this.telefone = telefone;
         this.email = email;
-        this.dataCadastro = new Date();
+        this.dataCadastro = LocalDate.now();
         this.totalGasto = 0.0;
         this.comprasRealizadas = 0;
     }
@@ -93,11 +94,11 @@ public class Cliente {
         this.totalGasto = totalGasto;
     }
 
-    public Date getDataCadastro() {
+    public LocalDate getDataCadastro() {
         return dataCadastro;
     }
 
-    public void setDataCadastro(Date dataCadastro) {
+    public void setDataCadastro(LocalDate dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
 }
--- src/main/java/git/desafioalexey/pizzaria/services/VendaService.java
+++ src/main/java/git/desafioalexey/pizzaria/services/VendaService.java
@@ -29,20 +29,6 @@ public class VendaService implements CrudInterface<Venda>{
 
     @Override
     public Venda criar(Venda venda) {
-        /*Pizza pizza = new Pizza(1L,"Chocolate", "Doce", 20.0);
-
-        ItemVenda item = new ItemVenda(1L, pizza, 2, 20.0);
-        ItemVenda item2 = new ItemVenda(1L, pizza, 5, 20.0);
-
-        Random gerar = new Random();
-        int numeroAleatorio = gerar.nextInt(99);
-
-        venda.setId(Long.valueOf(numeroAleatorio));
-        venda.setData(new Date());
-        venda.setItens(List.of(
-                item, item2
-        ));*/
-
         Cliente cliente = clienteService.listarPorId(venda.getCliente().getId());
         cliente.setComprasRealizadas(cliente.getComprasRealizadas() + 1);
 
@@ -61,46 +47,18 @@ public class VendaService implements CrudInterface<Venda>{
 
     @Override
     public Venda atualizar(Venda venda, Long id) {
-        /*
-        * Cliente clienteEncotrado = clienteRepository.findById(id).get();
+        Venda vendaEncontrada = vendaRepository.findById(id).get();
 
-        clienteEncotrado.setNome(cliente.getNome());
-        clienteEncotrado.setEndereco(cliente.getEndereco());
-        clienteEncotrado.setEmail(cliente.getEmail());
-        clienteEncotrado.setTelefone(cliente.getTelefone());
+        vendaEncontrada.setCliente(venda.getCliente());
 
-        clienteRepository.save(clienteEncotrado);
-
-        return clienteEncotrado;*/
+        vendaRepository.save(vendaEncontrada);
 
         return venda;
     }
 
     @Override
     public List<Venda> listarTodos() {
-        Cliente cliente1 = new Cliente(1L,"Alexey", "Guara 1", "61 999996666", "a@l.com");
-        Cliente cliente2 = new Cliente(2L,"Thanan", "Guara 1", "61 999996669", "t@l.com");
-
-        Pizza pizza = new Pizza(1L,"Chocolate", "Doce", 20.0);
-
-        ItemVenda item = new ItemVenda(1L, pizza, 2, 20.0);
-        ItemVenda item2 = new ItemVenda(1L, pizza, 5, 20.0);
-
-        Venda venda1 = new Venda(2L, LocalDate.now(),cliente1);
-        Venda venda2 = new Venda(3L, LocalDate.now(),cliente2);
-        Venda venda3 = new Venda(5L, LocalDate.now(),cliente1);
-
-        venda1.setItens(List.of(
-                item, item2
-        ));
-        venda2.setItens(List.of(
-                item
-        ));
-        venda3.setItens(List.of(
-                item2
-        ));
-
-        List<Venda> vendas = List.of(venda1, venda2, venda3);
+        List<Venda> vendas = vendaRepository.findAll();
 
         return vendas;
     }
