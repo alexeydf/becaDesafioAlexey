ad31c9413393d5f5751142222edc7667cf55fbae
Alexey Lima Barreto <alimabar@emeal.nttdata.com>
2022-02-04 10:38:06 -0300

    desafio 05

--- src/main/java/git/desafioalexey/pizzaria/services/PizzaService.java
+++ src/main/java/git/desafioalexey/pizzaria/services/PizzaService.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class PizzaService {
+public class  PizzaService {
 
     @Autowired
     private PizzaRepository pizzaRepository;
